8039debb6165f7d1457b4357bb81379f5e81f29a
Russell Macalintal <russell.macalintal@gmail.com>
2019-11-16 01:57:38 +0000

    Automatically backed up by Learn

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -2,7 +2,7 @@ module Players
 
   class Human < Player
     def move(board)
-      puts "#{self.token} Enter your move [1-9]:"
+      puts "#{self.token}: Enter your move [1-9]:"
       input = gets.strip
     end
   end
